ce15de24417e993141db857a1157319efdc4d8d5
Nan Wang <wangnan3@gmail.com>
2021-11-15 15:24:30 -0800

    guess number, break out and pass variable to function

---
+++ 100_day_course/day_12/guess_the_number.py
@@ -0,0 +1,39 @@
+from art import logo
+import random
+print(logo)
+
+RANDOM_NUMBER=random.randint(1,100)
+def compare(guess):
+    if guess<RANDOM_NUMBER:
+        print('too low')
+    elif guess>RANDOM_NUMBER:
+        print('too high')
+    elif guess==RANDOM_NUMBER:
+        print(f'You got it! The answer was {RANDOM_NUMBER}.')
+        return
+def game(times):
+    while True:
+        break_out_of_while = False
+        for i in range(times):
+            print(f'You have {times} attempts remaining to guess the number.')
+            guess=int(input('make a guess'))
+            compare(guess)
+            times -= 1
+            if guess == RANDOM_NUMBER:
+                break_out_of_while=True
+                break
+        if break_out_of_while:
+            break
+        
+def main():
+    print("Welcome to the Number Guessing Game!\n I'm thinking of a number between 1 and 100.")
+    print(f'Pssst, the correct answer is {RANDOM_NUMBER}')
+    level=input("Choose a difficulty. Type 'easy' or 'hard':\n")
+
+    if level == 'easy':
+        game(5)
+    if level == 'hard':
+        game(10)
+main()
+        
+
