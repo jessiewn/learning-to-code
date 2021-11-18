9cf3301168d83ba9d87a037bc72b1c8082d4c764
Nan Wang <wangnan3@gmail.com>
2021-11-17 21:44:29 -0800

    sencond version break problem solved

--- 100_day_course/day_14/first_version.py
+++ 100_day_course/day_14/first_version.py
@@ -1,40 +1,48 @@
 import random
 import data
+from art import logo
+from art import vs
 
-def game(answer):
+def game(guess):
     global score
     global A
     global B 
     if A['follower_count']> B['follower_count']:
-        if answer == 'A':
+        answer='A'
+        if guess == answer:
             score+=1
-            print(F'you are right, current score{score}')
+            print(f'you are right, current score{score}')
             B=random.choice(data.data)
             # ?what if random same B
-        elif answer == 'B':
+        elif guess != answer:
             print(f"Sorry, that's wrong. Final score: {score}")
-            return
+            answer='wrong'
+            return answer
     elif A['follower_count']< B['follower_count']:
-        if answer=='A':
+        answer='B'
+        if guess!=answer:
             print(f"Sorry, that's wrong. Final score: {score}")
-            return
-        elif answer=='B':
+            answer='wrong'
+            return answer
+        elif guess==answer:
             score+=1
             print(F'you are right, current score{score}')
             A=B
             B=random.choice(data.data)
-
+            
+print(logo)
 A=random.choice(data.data)
 B=random.choice(data.data) 
 score=0
 print(f"Compare A: {A['name']}, {A['description']}, from {A['country']}.")
-#print(vs)
-print(f"Compare B: {B['name']}, {B['description']}, from {B['country']}.")
+print(vs)
+print(f"Against B: {B['name']}, {B['description']}, from {B['country']}.")
 while True:
-    answer=input('what is your guess')
-    game(answer)
+    guess=input("Who has more followers? Type 'A' or 'B'\n")
+    if game(guess)== 'wrong':
+        break
     print(f"Compare A: {A['name']}, {A['description']}, from {A['country']}.")
-    ##print(vs)
-    print(f"Compare B: {B['name']}, {B['description']}, from {B['country']}.")
+    print(vs)
+    print(f"Against B: {B['name']}, {B['description']}, from {B['country']}.")
 
     
