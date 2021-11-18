4823d57d09910e5db6a813854a60da0f6c5f5acc
Nan Wang <wangnan3@gmail.com>
2021-11-18 14:17:44 -0800

    final version fix global scope problem and simplify code

--- 100_day_course/day_14/first_version.py
+++ 100_day_course/day_14/first_version.py
@@ -1,48 +1,46 @@
 import random
-import data
-from art import logo
-from art import vs
 
-def game(guess):
-    global score
-    global A
-    global B 
-    if A['follower_count']> B['follower_count']:
+import data
+import art
+
+def game(A,B,guess):
+    if A> B:
         answer='A'
-        if guess == answer:
-            score+=1
-            print(f'you are right, current score{score}')
-            B=random.choice(data.data)
-            # ?what if random same B
-        elif guess != answer:
-            print(f"Sorry, that's wrong. Final score: {score}")
-            answer='wrong'
-            return answer
-    elif A['follower_count']< B['follower_count']:
+    elif A< B:
         answer='B'
-        if guess!=answer:
-            print(f"Sorry, that's wrong. Final score: {score}")
-            answer='wrong'
-            return answer
-        elif guess==answer:
+
+    if guess!=answer:
+        return 'wrong'
+    elif guess==answer:
+        return 'right'
+
+    
+
+def main():
+    print(art.logo)
+    A=random.choice(data.data)
+    B=random.choice(data.data) 
+    # ?what if random same item
+    score=0
+    print(f"Compare A: {A['name']}, {A['description']}, from {A['country']}.")
+    print(art.vs)
+    print(f"Against B: {B['name']}, {B['description']}, from {B['country']}.")
+    while True:
+        guess=input("Who has more followers? Type 'A' or 'B'\n")
+        if game(A['follower_count'], B['follower_count'],guess)=='right':
             score+=1
             print(F'you are right, current score{score}')
-            A=B
-            B=random.choice(data.data)
-            
-print(logo)
-A=random.choice(data.data)
-B=random.choice(data.data) 
-score=0
-print(f"Compare A: {A['name']}, {A['description']}, from {A['country']}.")
-print(vs)
-print(f"Against B: {B['name']}, {B['description']}, from {B['country']}.")
-while True:
-    guess=input("Who has more followers? Type 'A' or 'B'\n")
-    if game(guess)== 'wrong':
-        break
-    print(f"Compare A: {A['name']}, {A['description']}, from {A['country']}.")
-    print(vs)
-    print(f"Against B: {B['name']}, {B['description']}, from {B['country']}.")
+            if A['follower_count']>B['follower_count']:
+                B=random.choice(data.data) 
+            elif A['follower_count']<B['follower_count']:
+                A=B
+                B=random.choice(data.data)
+        elif game(A['follower_count'], B['follower_count'],guess)=='wrong':
+            print(f"Sorry, that's wrong. Final score: {score}")
+            break
+        print(f"Compare A: {A['name']}, {A['description']}, from {A['country']}.")
+        print(art.vs)
+        print(f"Against B: {B['name']}, {B['description']}, from {B['country']}.")
+main()
 
     
